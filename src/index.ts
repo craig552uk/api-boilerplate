645475165f229dd891bf524bb53905f571383764
Craig Russell <craig@craig-russell.co.uk>
2018-01-19 21:41:24 +0000

    Added MongoDB connection logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as mongoose from "mongoose";
 import { argv } from "yargs";
 import { ApplicationService } from "./lib/application.service";
 import { LoggerService } from "./lib/logger.service";
@@ -5,10 +6,25 @@ import { LoggerService } from "./lib/logger.service";
 // Get CLI arguments
 const HOST = argv.host || "0.0.0.0";
 const PORT = argv.port || 1337;
+const DBURL = argv.DBURL || "mongodb://localhost/local";
 
-// Get Service Instances
+// Use native promises in Mongoose
+(mongoose as any).Promise = global.Promise;
+
+// Get Logger Instance
 const logger = LoggerService.getInstance();
-const app = ApplicationService.getInstance();
 
-// Serve Application
-app.listen(PORT, HOST, () => logger.info(`Listening on http://${HOST}:${PORT}`));
+// Connect to MongoDB
+mongoose.connect(DBURL)
+    .then(() => {
+        logger.info(`Connected to ${DBURL}`);
+
+        // Get Application instance
+        const app = ApplicationService.getInstance();
+
+        // Serve application
+        app.listen(PORT, HOST, () => {
+            logger.info(`Listening on http://${HOST}:${PORT}`);
+        });
+    })
+    .catch(logger.error);
